bb8fa8722c7987243e0dc7b33155b1befa6dd16d
zzt <zengzt93@gmail.com>
2017-12-25 08:28:47 +0800

    add comment and deep mnist start.

---
+++ tf_trial/deep_mnist.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+
+def deep():
+    sess = tf.InteractiveSession()
--- tf_trial/mnist.py
+++ tf_trial/mnist.py
@@ -2,7 +2,14 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
 
-def minst():
+def mnist():
+    """
+    construct a simple two layer NN:
+    - input layer with 784 neurons
+    - output layer with 10 neurons, using softmax as activation function
+    and cross entropy for error function
+    :return:
+    """
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
     # None means that a dimension can be of any length.
@@ -11,15 +18,15 @@ def minst():
     b = tf.Variable(tf.zeros([10]), dtype=tf.float32)
     # z = w * x + b
     z = tf.matmul(x, w) + b
-    yHat = tf.nn.softmax(z)
+    y_hat = tf.nn.softmax(z)
 
     # cost function
     y = tf.placeholder(tf.float32, [None, 10])
     # it is numerically unstable
-    # -tf.reduce_sum(tf.log(yHat) * y, reduction_indices=[1])
+    # -tf.reduce_sum(tf.log(y_hat) * y, reduction_indices=[1])
     # not use mean will reduce the accuracy to 0.5
-    # cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=yHat)
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=yHat))
+    # cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_hat)
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_hat))
 
     train = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
@@ -29,9 +36,10 @@ def minst():
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train, feed_dict={x: batch_xs, y: batch_ys})
 
-    correct_prediction = tf.equal(tf.argmax(yHat, 1), tf.argmax(y, 1))
+    correct_prediction = tf.equal(tf.argmax(y_hat, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
 
+
 if __name__ == '__main__':
-    minst()
+    mnist()
